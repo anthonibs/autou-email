2cd50ec5e4fdd21b51f0cb4b4c6be27e07d5df24
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2025-08-30 18:47:19 -0300

    feat: Adicionar funções para remoção de scripts maliciosos e formatação de tamanhos de arquivos

--- frontend/src/helpers/index.ts
+++ frontend/src/helpers/index.ts
@@ -4,3 +4,21 @@ export function formatDate(dateString: Date | string) {
     timeStyle: "short",
   }).format(new Date(dateString));
 }
+
+export function removeMaliciousScripts(input: string): string {
+  const scriptTagRegex =
+    /<\s*script\b[^>]*>[\s\S]*?<\/\s*script\s*>|<\s*script[^>]*\/\s*>|^\s*script\s*\/?>/gis;
+  return input.replace(scriptTagRegex, "");
+}
+
+export function formatFileSize(size?: number): string {
+  if (!size) return "0 B";
+
+  if (size >= 1_000_000) {
+    return `${(size / 1_000_000).toFixed(1)} MB`;
+  }
+  if (size >= 1_000) {
+    return `${(size / 1_000).toFixed(1)} kB`;
+  }
+  return `${size} B`;
+}
